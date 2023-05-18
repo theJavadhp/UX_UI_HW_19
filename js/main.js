8ab526c99791b1db412dca4ac4d378523abb4c78
theJavadhp <mjhp29@yahoo.com>
2023-05-18 11:12:42 -0400

    compeleting grid system

--- js/main.js
+++ js/main.js
@@ -1,25 +1,36 @@
-function openTab(evt, tabName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-  
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-  
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
 
-    document.getElementById("defaultOpen").click();
 
-  }
+$(function () {
+  $(".tech-slider").infiniteslide({
+    speed: 40,
+    direction: "right",
+    responsive: true,
+    pauseonhover: false,
+  });
+});
+
+$(function () {
+  $(".skill-scroll1").infiniteslide({
+    speed: 15,
+    direction: "left",
+    pauseonhover: false,
+  });
+});
+
+$(function () {
+  $(".skill-scroll2").infiniteslide({
+    speed: 20,
+    direction: "right",
+    pauseonhover: false,
+  });
+});
+
+$(function () {
+  $(".skill-scroll3").infiniteslide({
+    speed: 20,
+    direction: "left",
+    pauseonhover: false,
+  });
+});
 
 
